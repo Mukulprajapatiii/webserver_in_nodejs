b25c5395a4a86e5120bb927dc7e33a08262f5fb5
mukulprajapatiii <mukulprajapati1319@gmail.com>
2024-07-04 16:05:31 +0530

    4julycommit

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const http = require("http");
+const fs = require("fs");
 
 const myserver =http.createServer((req, res)=>{
-    console.log("new req created :)");
-    res.end("Hello from server");
+    const log = `${Date.now()}: ${req.url} New Request Recieved\n`;
+    fs.appendFile("log.txt",log, (err, data)=>{
+        
+        switch(req.url){
+            case`/`: res.end("Home Page");
+            break
+            case`/about`: res.end("I am Mukul P.");
+            default: res.end("No valid URL");
+        }
+        
+    })
+    
 });
 
-myserver.listen(8000, () => console.log("Server Started "))
+myserver.listen(8000, () => console.log("Server Started "));     
